ea52e8509453d38501a8cf0dd2e6aca53aa18343
ShawonBit <hossain.ict@gmail.com>
2021-04-09 16:13:36 +0600

    add baseurl Constant in environment.prod.ts

--- geolocation-angular/src/app/services/auth.service.ts
+++ geolocation-angular/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { baseUrl } from '../../environments/environment';
+import { baseUrl } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
--- geolocation-angular/src/environments/environment.prod.ts
+++ geolocation-angular/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true
 };
+export const baseUrl='https://nameless-plains-51062.herokuapp.com/geolocation'
